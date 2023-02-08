c5a0d7fc8ca96aef0bb1941fdfc8869f7ed4805b
Cristian Valenti <122291593+CyberCribbio@users.noreply.github.com>
2023-02-08 17:17:18 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        for(int i = 5; i <= 25; ++i) {
+            System.out.println(i);
+        }
+
+    }
+}
